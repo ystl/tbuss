94f147e2958472e3530350194a9ce8420dce8cfa
Administrator <1253356807@qq.com>
2019-10-08 15:34:11 +0800

    Initial commit

---
+++ src/main/java/com/td/tbuss/TbussApplication.java
@@ -0,0 +1,13 @@
+package com.td.tbuss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TbussApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TbussApplication.class, args);
+    }
+
+}
